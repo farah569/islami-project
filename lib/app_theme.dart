6f5fa764e7ee95901203f46f6a99d7d3a54690e1
farah <faarh50701@gmail.com>
2024-10-10 00:45:52 +0300

    Finish home screen

---
+++ lib/app_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+ class AppTheme {
+
+static const Color lightPrimary =Color(0xFFB7935F);
+static const  Color darkPrimary = Color(0xFF141A2E);
+static const Color white =Color(0xFFF8F8F8);
+static const Color dark =Color(0xFF242424);
+static const Color gold =Color(0xFFFACC1D);
+
+static ThemeData lightTheme =ThemeData(primaryColor: lightPrimary,
+appBarTheme: AppBarTheme(
+  centerTitle: true,
+  backgroundColor: Colors.transparent,
+  titleTextStyle: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  )
+),
+scaffoldBackgroundColor: Colors.transparent,
+bottomNavigationBarTheme: BottomNavigationBarThemeData(
+  type: BottomNavigationBarType.fixed,
+  backgroundColor: lightPrimary,
+  selectedItemColor: Colors.black,
+  unselectedItemColor: white,
+)
+
+);
+
+
+
+
+static ThemeData darkTheme =ThemeData(primaryColor: darkPrimary);
+
+ 
+
+
+
+
+
+ }
